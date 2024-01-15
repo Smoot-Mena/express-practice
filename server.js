688772afa92a33a605f45c2887cc127a8e7019b9
Smoot-Mena <Msmoot810@gmail.com>
2024-01-14 21:21:26 -0500

    adds magic 8 ball app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let colors = ["red", "blue", "green", "orange"];
+let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
 const express = require("express");
 const port = 3216;
@@ -6,7 +7,7 @@ const port = 3216;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(`<h1>LINKS TO APPS</h1><br /><br /><a href="/home">Home Page</a><br /><br /><a href="/greeting/:name">Greeting Page</a><br /><br /><a href="/colors/:indexOfColor">Color Page</a><br /><br /><a href="/hello/:firstname/:lastname">Hello Page</a><br /><br /><a href="/tip/:total/:tipPercentage">Tip Calculator</a>`);
+    res.send(`<h1>LINKS TO APPS</h1><br /><br /><a href="/home">Home Page</a><br /><br /><a href="/greeting/:name">Greeting Page</a><br /><br /><a href="/colors/:indexOfColor">Color Page</a><br /><br /><a href="/hello/:firstname/:lastname">Hello Page</a><br /><br /><a href="/tip/:total/:tipPercentage">Tip Calculator</a><br /><br /><a href="/magic/:question">Magic 8 Ball</a>`);
 });
 
 app.get("/home", (req, res) => {
@@ -31,6 +32,11 @@ app.get("/hello/:firstname/:lastname", (req, res) => {
 
 app.get("/tip/:total/:tipPercentage", (req, res) => {
     res.send(`<h1>Your calculated tip is: $${(req.params.total * req.params.tipPercentage / 100)}</h1>`);
+});
+
+app.get("/magic/:question", (req, res) => {
+    let randomAnswer = Math.floor(Math.random() * answers.length);
+    res.send(`<h1>${req.params.question}</h1> <p>${answers[randomAnswer]}</p>`);
 });
 
 // app.get("/:test", (req, res) => {
